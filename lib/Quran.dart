43a54c757d585fa60db3f517a0cc0b970817bb4a
unknown <houssemeddinesouissi10@gmail.com>
2020-01-11 21:28:33 +0100

    feat: add Bottom Navigation

--- lib/Quran.dart
+++ lib/Quran.dart
@@ -1,9 +1,6 @@
-import 'dart:ui';
-
 import 'package:device_info/device_info.dart';
 import 'package:flutter/material.dart';
 import 'package:native_pdf_view/native_pdf_view.dart';
-
 
 void main() => runApp(Quran());
 
@@ -14,107 +11,144 @@ class Quran extends StatefulWidget {
 
 class _QuranState extends State<Quran> {
   PDFDocument _document;
-  static const List<double> _doubleTapScales = <double>[1.0, 2.0];
+  static const List<double> _doubleTapScales = <double>[1.0, 1.1];
   int currentPage;
-  final pageController = PageController(
-    initialPage: 569,
-      viewportFraction: 1,
-      keepPage: true
-  );
+  final pageController =
+      PageController(initialPage: 569, viewportFraction: 1, keepPage: true);
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: الفهرس',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-    theme: ThemeData(primaryColor: Colors.white),
-    home: Scaffold(
-     // appBar: AppBar(title: Text('PDFView example')),
-      body: FutureBuilder<PDFDocument>(
-        future: _getDocument(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Stack(
-              children: <Widget>[
-                PDFView.builder(
-                  key: UniqueKey(),
-                  scrollDirection: Axis.horizontal,
-                  document: snapshot.data,
-                  controller: pageController,
-                  builder: (PDFPageImage pageImage, bool isCurrentIndex) {
-                    Widget image = ExtendedImage.memory(
-                      pageImage.bytes,
-                      fit: BoxFit.fitWidth,
-                      mode: ExtendedImageMode.gesture,
-                      initGestureConfigHandler: (_) => GestureConfig(
-                        minScale: 1,
-                        animationMinScale: .75,
-                        maxScale: 2,
-                        animationMaxScale: 2.5,
-                        speed: 1,
-                        inertialSpeed: 100,
-                        inPageView: true,
-                        initialScale: 1.2,
-                        cacheGesture: false,
+        theme: ThemeData(primaryColor: Colors.white),
+        home: Scaffold(
+          // appBar: AppBar(title: Text('PDFView example')),
+          body: FutureBuilder<PDFDocument>(
+            future: _getDocument(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return SafeArea(
+                  child: Stack(
+                    children: <Widget>[
+                      PDFView.builder(
+                        scrollDirection: Axis.horizontal,
+                        document: snapshot.data,
+                        controller: pageController,
+                        builder: (PDFPageImage pageImage, bool isCurrentIndex) {
+                          Widget image = ExtendedImage.memory(
+                            pageImage.bytes,
+                            fit: BoxFit.fitWidth,
+                            // gesture not applied (minScale,maxScale,speed...)
+                            mode: ExtendedImageMode.gesture,
+                              initGestureConfigHandler: (_) => GestureConfig(
+                        //minScale: 1,
+                         // animationMinScale:1,
+                         // maxScale: 1.1,
+                          //animationMaxScale: 1,
+                          speed: 1,
+                          inertialSpeed: 100,
+                          inPageView: true,
+                          initialScale: 1,
+                          cacheGesture: true,
+                        ),
+                            onDoubleTap: (ExtendedImageGestureState state) {
+                              final pointerDownPosition =
+                                  state.pointerDownPosition;
+                              final begin = state.gestureDetails.totalScale;
+                              double end;
+                              if (begin == _doubleTapScales[0]) {
+                                end = _doubleTapScales[1];
+                              } else {
+                                end = _doubleTapScales[0];
+                              }
+                              state.handleDoubleTap(
+                                scale: end,
+                                doubleTapPosition: pointerDownPosition,
+                              );
+                            },
+                          );
+                          if (isCurrentIndex) {
+                            image = Hero(
+                              tag: 'pdf_view' + pageImage.pageNumber.toString(),
+                              child: Container(child: image),
+                              transitionOnUserGestures: true,
+                            );
+                          }
+                          return image;
+                        },
                       ),
-                      onDoubleTap: (ExtendedImageGestureState state) {
-                        final pointerDownPosition = state.pointerDownPosition;
-                        final begin = state.gestureDetails.totalScale;
-                        double end;
 
-                         if (begin == _doubleTapScales[0]) {
-                          end = _doubleTapScales[1];
-                        } else {
-                          end = _doubleTapScales[0];
-                        }
-                        state.handleDoubleTap(
-                          scale: end,
-                          doubleTapPosition: pointerDownPosition,
+                    ],
+                  ),
+                );
 
-                        );
-                      },
+              }
 
-                    );
-                    if (isCurrentIndex) {
-                      image = Hero(
-                        tag: 'pdf_view' + pageImage.pageNumber.toString(),
-                        child: image,
-                      );
-                    }
-
-                    return image;
-                  },
-
-                ),
-                /* Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        '$_actualPageNumber/${snapshot.data.pagesCount}',
-                        style: TextStyle(fontSize: 34),
-                      ),
-                    )*/
-              ],
-            );
-          }
-
-          if (snapshot.hasError) {
-            return Center(
-              child: Text(
-                'PDF Rendering does not '
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    'PDF Rendering does not '
                     'support on the system of this version',
+                  ),
+                );
+              }
+
+              return Center(child: CircularProgressIndicator());
+            },
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text('الفهرس'),
               ),
-            );
-          }
-
-          return Center(child: CircularProgressIndicator());
-        },
-      ),
-    ),
-  );
-
+              BottomNavigationBarItem(
+                icon: Icon(Icons.business),
+                title: Text('حفظ علامة'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.school),
+                title: Text('الإنتقال إلى العلامة'),
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.amber[800],
+            onTap: _onItemTapped,
+          ),
+        ),
+      );
 
   Future<bool> hasSupport() async {
     DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
     AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
     bool hasSupport = androidInfo.version.sdkInt >= 21;
     return hasSupport;
+  }
+
+  void currentPositionUpdate() {
+    setState(() {
+      currentPage = pageController.page.toInt();
+    });
   }
 
   Future<PDFDocument> _getDocument() async {
@@ -126,7 +160,7 @@ class _QuranState extends State<Quran> {
     } else {
       throw Exception(
         'PDF Rendering does not '
-            'support on the system of this version',
+        'support on the system of this version',
       );
     }
   }
